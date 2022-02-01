deaa60bd115c7094d8f183dc9b88b18a15708130
Despert Ange <ange.despert@gmail.com>
2022-02-01 21:32:47 +0100

    Création de la structure personnage

---
+++ src/personnage.c
@@ -0,0 +1,25 @@
+/**
+ * \file personnage.c
+ * \author Despert Ange (Ange.Despert.Etu@univ-lemans.fr)
+ * \brief Fichier contenant toutes les fonctions concernant le personnage
+ * \version 0.1
+ * \date 01/02/2022
+ * 
+ * \copyright Copyright (c) 2022
+ * 
+ */
+
+/**
+ * \struct struct joueur
+ * \brief Structure non manipulable hors des fonctions du personnage contenant les informations sur le joueur
+ * 
+ * \author Despert Ange
+ */
+struct joueur
+{
+	char nom_pers[20]; /**<Le nom du personnage*/
+	short int niveau; /**<Le niveau du joueur*/
+	int xp; /**<Le nombre de points d'expérience que possède le joueur */
+	long trigger; /**<Une variable contenant des triggers logiques concernant le personnage */
+	/** TODO : créer un type énuméré map*/
+};
