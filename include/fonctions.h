9fbad6952a796ca91fef0e9ab14710b696fc11c2
TheMisterPenguin <Ange.Despert.Etu@univ-lemans.fr>
2022-02-02 22:55:38 +0100

    Ajout de #ifndef et ajout de la fonction jeu_event

--- include/fonctions.h
+++ include/fonctions.h
@@ -1,4 +1,17 @@
-void init(void);
+/**
+ * \file fonctions.h
+ * \author Despert Ange (Ange.Despert.Etu@univ-lemans.fr)
+ * \brief Fichier qui contient les définitions de toutes les fonctions
+ * \version 0.1
+ * \date 02/02/2022
+ * 
+ * \copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef H_FONCTION
+#define H_FONCTION
+extern void init(void);
 /**
  * \fn void init(void);
  * \brief Fonction qui initialise le Programme
@@ -10,4 +23,13 @@ void init(void);
  * \brief Fonction qui appelle les fonctions pour terminer le programme
  * \author Ange Despert
  */
-void terminate(void);
+extern void terminate(void);
+
+/**
+ * \fn void jeu_event(void)
+ * \brief Fonction qui gère les événements
+ * \author Despert Ange
+ * 
+ */
+extern void jeu_event(void);
+#endif
