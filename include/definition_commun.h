906a178a0b4a096f5a55c68d3b83391eabcaab40
TheMisterPenguin <Ange.Despert.Etu@univ-lemans.fr>
2022-02-03 17:02:20 +0100

    Ajout du 'save Path' et le pointeur commun joueur

--- include/definition_commun.h
+++ include/definition_commun.h
@@ -1,4 +1,5 @@
 #include "SDL2/SDL.h"
+#include "personnage.h"
 /** 
  * \file definition_commun.h
  * \brief Contient toutes les définitions communes à tout les fichiers
@@ -11,6 +12,8 @@
 #define __H_DEF_COMMUN__
 extern SDL_Window *fenetre_Principale; /**Pointeur vers fenêtre globale pointant sur la fenêtre principale du programme*/
 extern SDL_Renderer *rendu_principal;
+extern joueur_t *joueur; /**<Pointeur vers la structure du joueur principal*/
+
 #define FENETRE_LONGUEUR 640
 #define FENETRE_LARGEUR 480
 
@@ -22,4 +25,6 @@ typedef struct {
 }t_game;
 
 extern t_game game;
+
+#define SAVE_PATH "~/Games/Bloody Satana/"
 #endif
