85670a8f87f211cb895ff94cfc3e89da1cc3e25a
TheMisterPenguin <Ange.Despert.Etu@univ-lemans.fr>
2022-02-03 14:00:14 +0100

    Ajout de la prise en compte des touches de déplacement

---
+++ include/event.h
@@ -0,0 +1,19 @@
+/**
+ * \file event.h
+ * \author Despert Ange (Ange.Despert.Etu@univ-lemans.fr)
+ * \brief 
+ * \version 0.1
+ * \date 03/02/2022
+ * 
+ * \copyright Copyright (c) 2022
+ * 
+ */
+#include "SDL2/SDL_events.h"
+#define TOUCHE_HAUT SDLK_z
+#define TOUCHE_BAS SDLK_s
+#define TOUCHE_GAUCHE SDLK_q
+#define TOUCHE_DROITE SDLK_d
+
+
+
+
