8fb98118a9c7977c1b3994dd2596976d60cb7c84
TheMisterPenguin <Ange.Despert.Etu@univ-lemans.fr>
2022-02-11 18:46:43 +0100

    test de la création d'un thread

--- src/main.c
+++ src/main.c
@@ -5,17 +5,32 @@ SDL_Window *fenetre_Principale = NULL;
 SDL_Renderer *rendu_principal = NULL;
 t_game game;
 
+int t_affichage(void *ptr){
 
+    for(int i = 0; i < 10; i++)
+        printf("Cet affichage est dans le thread affichage !\n");
+
+    return 0;
+}
 
 int main(int argc, char** argv)
 {
+    SDL_Thread *p_t_affichage;
+    int val_retour;
+
+    p_t_affichage = SDL_CreateThread(t_affichage, "Gestion de l'affichage du programme", NULL);
     /* Initialisation simple */
     
-    printf("test\n");
+    for(int i = 0; i < 15; i++)
+        printf("%i\n", i);
+
+    SDL_WaitThread(p_t_affichage, &val_retour);
+
+    /*printf("test\n");
     init();
 
     while(SDL_TRUE){
         jeu_event();
-    }
+    }*/
 
 }
