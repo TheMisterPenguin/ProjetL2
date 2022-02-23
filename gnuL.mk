ae901c16d7b3f44324594049e84ed2c8f902695f
TheMisterPenguin <74404918+TheMisterPenguin@users.noreply.github.com>
2022-02-23 16:05:11 +0200

    ajout de libm

--- gnuL.mk
+++ gnuL.mk
@@ -1,4 +1,4 @@
 INCLUDE = $(addprefix -I , include)
-LDFLAGS = -L lib -lSDL2 # Options d'édition de liens. 
+LDFLAGS = -L lib -lSDL2  -l libm# Options d'édition de liens. 
 GEN = jeux.prog # Executables
 ODIRCMD = obj/
