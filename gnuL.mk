884cd63534856320a884ac7c7356e62fdadd125c
TheMisterPenguin <74404918+TheMisterPenguin@users.noreply.github.com>
2022-02-24 14:32:36 +0200

    Installation de la librairie json-c

--- gnuL.mk
+++ gnuL.mk
@@ -1,4 +1,4 @@
 INCLUDE = $(addprefix -I , include)
-LDFLAGS = -L lib -lSDL2  -l m# Options d'édition de liens. 
+LDFLAGS = -L lib -lSDL2 -ljson-c -l m# Options d'édition de liens. 
 GEN = jeux.prog # Executables
 ODIRCMD = obj/
