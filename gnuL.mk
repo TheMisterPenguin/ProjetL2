710f7ef292820dd3510deabf8211f07572fe037c
Anged <you@examples.com>
2022-03-03 20:45:09 +0100

    Suppresion acidentelle lors du merge précédent

--- gnuL.mk
+++ gnuL.mk
@@ -1,4 +1,4 @@
 INCLUDE = $(addprefix -I , include)
 GEN = $(addprefix $(BINDIR), jeux.prog) # Executables
-LDFLAGS = -L lib -lSDL2  -l m# Options d'édition de liens. 
+LDFLAGS = -L lib -lSDL2  -l json-c -l m# Options d'édition de liens. 
 ODIRCMD = obj/
