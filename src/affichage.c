2efc10cea9e95558fa8caabd42dac930270104ac
varix33 <antoine.bruneau@outlook.fr>
2022-03-14 11:31:09 +0100

    modification de la fonction next_frame_joueur

--- src/affichage.c
+++ src/affichage.c
@@ -85,6 +85,19 @@ void next_frame_y(t_aff *texture){
         texture->frame_anim->y = 0; 
 }
 
+err_t next_frame_x_indice(t_aff *texture, const unsigned int indice)
+{
+    int temp = texture->frame_anim->x;
+    texture->frame_anim->x = (texture->frame_anim->w) * indice; /* On met à jour */
+
+    if (!rect_correct_texture(texture->frame_anim, texture->width, texture->height)){ /* Indice trop grand */
+        texture->frame_anim->x = temp;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
 err_t next_frame_y_indice(t_aff *texture, const unsigned int indice)
 {
     int temp = texture->frame_anim->y;
@@ -161,35 +174,100 @@ err_t afficher_texture(t_aff *texture, SDL_Renderer *rendu){
     t_l_aff* textures_joueur = malloc(sizeof(t_l_aff));
     textures_joueur->nb_valeurs = NB_SPRITE_JOUEUR;
     textures_joueur->liste = malloc(sizeof(t_aff)*NB_SPRITE_JOUEUR);
-    textures_joueur->liste[0] = creer_texture(N_T_MARCHER, 48, 48, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
-    textures_joueur->liste[1] = creer_texture(N_T_ATTAQUE, 48, 48, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
-    textures_joueur->liste[2] = creer_texture(N_T_ATTAQUE_CHARGEE, 48, 48, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
-    textures_joueur->liste[3] = creer_texture(N_T_CHARGER, 48, 48, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
-    textures_joueur->liste[4] = creer_texture(N_T_MARCHER_BOUCLIER, 48, 48, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
+    textures_joueur->liste[TEXT_MARCHER] = creer_texture(N_T_MARCHER, LARGEUR_PERSONNAGE, LONGUEUR_PERSONNAGE, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
+    textures_joueur->liste[TEXT_ATTAQUE] = creer_texture(N_T_ATTAQUE, LARGEUR_PERSONNAGE, LONGUEUR_PERSONNAGE, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
+    textures_joueur->liste[TEXT_ATTAQUE_CHARGEE] = creer_texture(N_T_ATTAQUE_CHARGEE, LARGEUR_PERSONNAGE, LONGUEUR_PERSONNAGE, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
+    textures_joueur->liste[TEXT_CHARGER] = creer_texture(N_T_CHARGER, LARGEUR_PERSONNAGE, LONGUEUR_PERSONNAGE, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
+    textures_joueur->liste[TEXT_MARCHER_BOUCLIER] = creer_texture(N_T_MARCHER_BOUCLIER, LARGEUR_PERSONNAGE, LONGUEUR_PERSONNAGE, 150, 150, (FENETRE_LONGUEUR * 0.022f) / 16 * 3);
+    /* positionnement au dernier sprite*/
+    next_frame_x_indice(textures_joueur->liste[TEXT_CHARGER], 2);
     return textures_joueur;
 }
 
-t_aff * next_frame_joueur(t_l_aff* textures_joueur){
-    if(perso_principal->statut->action == RIEN){
-        if(!perso_principal->statut->en_mouvement)
-            if(perso_principal->statut->bouclier_equipe)
-                return textures_joueur->liste[TEXT_MARCHER_BOUCLIER];
-            else
-                return textures_joueur->liste[TEXT_MARCHER];
-        else{
-            if(perso_principal->statut->bouclier_equipe){
-                next_frame_y_indice(textures_joueur->liste[TEXT_MARCHER_BOUCLIER], perso_principal->statut->orientation);
-                next_frame_x(textures_joueur->liste[TEXT_MARCHER_BOUCLIER]);
-                return textures_joueur->liste[TEXT_MARCHER_BOUCLIER];
+t_aff* init_texture_joueur(t_l_aff* textures_joueur){
+    /* if sauvegarde existante -> return texture sauvegardée du joueur*/
+    if(perso_principal->statut->bouclier_equipe)
+        return textures_joueur->liste[TEXT_MARCHER_BOUCLIER];
+    else
+        return textures_joueur->liste[TEXT_MARCHER];
+}
+
+t_aff* next_frame_joueur(t_l_aff* textures_joueur){
+    statut_t* statut = perso_principal->statut;
+    t_aff** textures = textures_joueur->liste;
+    int pause = 0;
+
+    if(statut->duree>0 && (compteur%5) == 0)
+        (statut->duree)--;
+
+
+    if(statut->action == ATTAQUE_OU_CHARGER && statut->duree == 0)
+                statut->action = CHARGER;
+    if( (statut->action == RIEN  || statut->action == ATTAQUE_OU_CHARGER) && statut->en_mouvement){
+        if( (compteur%5) == 0) /*compteur%5 pour la vitesse d'affichage*/
+            if(statut->bouclier_equipe){
+                next_frame_y_indice(textures[TEXT_MARCHER_BOUCLIER], statut->orientation);
+                next_frame_x(textures[TEXT_MARCHER_BOUCLIER]);
+                return textures[TEXT_MARCHER_BOUCLIER];
             }
             else{
-                next_frame_y_indice(textures_joueur->liste[TEXT_MARCHER], perso_principal->statut->orientation);
-                next_frame_x(textures_joueur->liste[TEXT_MARCHER]);
-                return textures_joueur->liste[TEXT_MARCHER];
+                next_frame_y_indice(textures[TEXT_MARCHER], statut->orientation);
+                next_frame_x(textures[TEXT_MARCHER]);
+                return textures[TEXT_MARCHER];
             }
+        else
+            pause = 1;
+    }
+    else{
+        if(statut->action == BLOQUER){
+            //bloquer les coups
+        }
+        else if(statut->action == CHARGER){ 
+            if((compteur%5) == 0){ /*compteur%5 pour la vitesse d'affichage*/
+                next_frame_x(textures[TEXT_CHARGER]);
+                if(statut->en_mouvement)
+                    next_frame_y_indice(textures[TEXT_CHARGER], 2 * (statut->orientation) + (textures[TEXT_CHARGER]->frame_anim->x) / (int) LONGUEUR_PERSONNAGE );
+                else
+                    next_frame_y_indice(textures[TEXT_CHARGER], 2 * (statut->orientation) );
+                return textures[TEXT_CHARGER];
+            }
+            else
+                pause = 1;
+        }
+        else if(statut->action == ATTAQUE){
+            if( (compteur%4) == 0){ /*compteur%4 pour la vitesse d'affichage*/
+                next_frame_x(textures[TEXT_ATTAQUE]);
+                next_frame_y_indice(textures[TEXT_ATTAQUE], statut->orientation);
+                /*si il a fait le tour du fichier sprite attaque, l'action est terminée*/
+                if( (textures[TEXT_ATTAQUE]->frame_anim->x) == (LONGUEUR_PERSONNAGE*2) )
+                    statut->action = RIEN;
+                return textures[TEXT_ATTAQUE];
+            }
+            else
+                pause = 1;
+        }
+        else if(statut->action == ATTAQUE_CHARGEE){
+            if( (compteur%3) == 0){ /*compteur%3 pour la vitesse d'affichage*/
+                //lorseque l'on rentre pour la première fois dans cette phase d'attaque chargée
+                if(statut->duree == (DUREE_ATTAQUE_CHARGEE-1))
+                    next_frame_x_indice(textures[TEXT_ATTAQUE_CHARGEE], (statut->orientation)*2 - 1 );
+                next_frame_x(textures[TEXT_ATTAQUE_CHARGEE]);
+                /*si il a fait le tour du fichier sprite attaque, l'action est terminée*/
+                if( ( (textures[TEXT_ATTAQUE_CHARGEE]->frame_anim->x) == (statut->orientation)*2*LONGUEUR_PERSONNAGE) && (statut->duree != (DUREE_ATTAQUE_CHARGEE-1) ) )
+                    statut->action = RIEN;
+                return textures[TEXT_ATTAQUE_CHARGEE];
+            }
+            else
+                pause = 1;
         }
     }
-    
+    /*si aucune des conditions*/
+    if(pause)
+        return NULL;
+    else if(statut->bouclier_equipe)
+        return textures[TEXT_MARCHER_BOUCLIER];
+    else
+        return textures[TEXT_MARCHER];
 }
 
 
