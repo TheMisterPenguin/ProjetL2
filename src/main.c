a2ddec459ac437297c4ad17d7dd8e2d50613c573
varix33 <antoine.bruneau@outlook.fr>
2022-03-14 11:32:16 +0100

    modification affichage joueur

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 SDL_Window *fenetre_Principale = NULL;
 SDL_Renderer *rendu_principal = NULL;
 bool running = vrai;
+long int compteur;
+
 #define NB_FPS 30
 
 void afficher_intro(void){
@@ -62,31 +64,35 @@ int t_affichage(void *ptr){
     int debut, fin; /* le temps pour calculer les performances */
     int i;
     
-    t_l_aff* textures_joueur = init_textures_joueur();
-    t_aff* next_texture_joueur;
+    perso_principal = creer_joueur("test");
+    
+    t_l_aff* textures_joueur = init_textures_joueur();  /* initialise la liste de textures joueur*/
+    t_aff* next_texture_joueur = init_texture_joueur(textures_joueur); /* initialise la texture joueur à afficher*/
+    t_aff* texture_temp;
 
+    /*test de l'allocation des textures*/
     for(i=0; i<textures_joueur->nb_valeurs; i++)
         if(textures_joueur->liste == NULL)
             exit(EXIT_FAILURE);
-    perso_principal = creer_joueur("test");
 
-    i=0;
+    compteur = 0;
     while (running)
     {   
         debut = SDL_GetPerformanceCounter();
-        if(i%5 == 0)
-            next_texture_joueur = next_frame_joueur(textures_joueur);
-        //next_frame_y_indice(text, perso_principal->orientation);
+
+        texture_temp = next_frame_joueur(textures_joueur);
+        if(texture_temp)
+            next_texture_joueur = texture_temp;
+
         SDL_RenderClear(rendu_principal);
         afficher_texture(next_texture_joueur, rendu_principal);
         SDL_RenderPresent(rendu_principal);
-        //next_frame_x(text);
 
         fin = SDL_GetPerformanceCounter();
 
         float temps_passe = (debut - fin) / (float)SDL_GetPerformanceFrequency();
-        SDL_Delay(floor((1000 / (float) NB_FPS) - temps_passe));
-        i++;
+        SDL_Delay(floor((1000 / (float) 60) - temps_passe));
+        compteur++;
     }
     return 0;
 }
@@ -94,7 +100,6 @@ int t_affichage(void *ptr){
 int main(int argc, char** argv)
 {
     SDL_Thread *f_t_aff;
-    printf("bonjour\n\n");
     init();
     afficher_intro();
     f_t_aff = SDL_CreateThread(t_affichage,"thread affichage", NULL);
