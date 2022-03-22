7e5be2e9687df8b01a7306855d14d61538d5b507
RaffouIUT <rafa72don@gmail.com>
2022-03-22 16:21:24 +0100

    Différents menus + début création fonction affichage

--- src/menus.c
+++ src/menus.c
@@ -1,3 +1,14 @@
 #include <stdlib.h>
 #include <commun.h>
 #include <string.h>
+
+void afficher_menu(menus_t * menu){
+    switch (menu) {
+        case 0: break;
+        case 1: break;
+        case 2: break;
+        case 3: break;
+    
+        default: exit(EXIT_SUCCESS);
+    }
+}
