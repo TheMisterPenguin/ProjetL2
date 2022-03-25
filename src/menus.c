98af99007904103c4ea304d7a4ce3fb6a84cbbe0
anged <you@examplse.com>
2022-03-25 10:01:35 +0100

    Ajout du menu pause

--- src/menus.c
+++ src/menus.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
-#include <commun.h>
 #include <string.h>
+#include <affichage.h>
 
-void afficher_menu(menus_t * menu){
+/*void afficher_menu(menus_t * menu){
     switch (menu) {
         case 0: break;
         case 1: break;
@@ -11,4 +11,43 @@ void afficher_menu(menus_t * menu){
     
         default: exit(EXIT_SUCCESS);
     }
+}*/
+
+void afficher_menu_pause(){
+
+    int debut, fin; /* le temps pour calculer les performances */
+    t_aff * text_pause = creer_texture("ressources/background/menu/Sprite_Menu_Pause.bmp", -1, -1, 0, 0, 0);
+
+    if(!text_pause){
+        return;
+    }
+    while(1){
+        debut = SDL_GetPerformanceCounter();
+        SDL_RenderClear(rendu_principal);
+
+        afficher_texture(text_pause, rendu_principal);
+
+        SDL_RenderPresent(rendu_principal);
+        SDL_Event lastEvent; /* On récupère les événements */
+
+        while (SDL_PollEvent(&lastEvent))
+        {
+            switch (lastEvent.type)
+            {
+            case SDL_QUIT:
+                printf("Détection de la fermeture de la fenêtre\n");
+                exit(EXIT_SUCCESS);
+            case SDL_KEYDOWN:
+                if (lastEvent.key.keysym.sym == SDLK_ESCAPE)
+                    return;
+                break;
+            case SDL_MOUSEBUTTONUP:
+                break;
+            }
+        }
+        fin = SDL_GetPerformanceCounter();
+        float temps_passe = (debut - fin) / (float)SDL_GetPerformanceFrequency();
+        SDL_Delay(floor((1000 / (float)60) - temps_passe));
+        compteur++;
+        }
 }
