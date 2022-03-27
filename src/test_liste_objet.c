1161ce98660b886ab7f5c9959b1fb234a0edb513
lederax <lederaxb@hotmail.com>
2022-03-27 12:01:19 +0200

    -Correction primitives liste

--- src/test_liste_objet.c
+++ src/test_liste_objet.c
@@ -10,13 +10,13 @@ unsigned int FENETRE_LONGUEUR, FENETRE_LARGEUR;
 int main(){
     lobjet_t * liste_obj = NULL;
 
-    printf("-- Création de la liste d'objets --\n");
+    printf("----- Création de la liste d'objets selon le fichier correspondant -----\n\n");
     liste_obj = creer_liste_objet();
 
-    printf("-- Affichage de la liste d'objets --\n");
+    printf("----- Affichage de la liste d'objets -----\n\n");
     afficher_liste_objet(liste_obj);
 
-    printf("-- Destruction de la liste d'objets --\n");
+    printf("----- Destruction de la liste d'objets -----\n\n");
     detruire_liste_objet(&liste_obj);
 
     return 0;
