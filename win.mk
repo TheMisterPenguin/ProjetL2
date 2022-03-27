69f1b01860f0216ea1d99ece391782b0f1d648cd
TheMisterPenguin <74404918+TheMisterPenguin@users.noreply.github.com>
2022-03-27 16:58:23 +0200

    Ajout des icones du jeux

--- win.mk
+++ win.mk
@@ -3,4 +3,4 @@ GEN = jeux.exe # Executables
 LDFLAGS = -L lib_win -l json-c -lmingw32 -lSDL2main -lSDL2# Options d'édition de liens. 
 ODIRCMD = obj/
 EXT = .exe
-JSON-LIB-PATH = 
+JSON-LIB-PATH = info.res icone.res
