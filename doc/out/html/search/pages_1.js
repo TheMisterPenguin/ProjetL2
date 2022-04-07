b4a208267a925535024c7a4585153ed38ac0f3fe
TheMisterPenguin <74404918+TheMisterPenguin@users.noreply.github.com>
2022-04-07 10:02:02 +0200

    MAJ doc

---
+++ doc/out/html/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['page_20principale_1093',['Page principale',['../index.html',1,'']]]
+];
--- include/commun.h
+++ include/commun.h
@@ -31,3 +31,41 @@
 extern void init(void);
 #endif
 
+/** \mainpage 
+ * # ProjetL2 Bloody Sanada
+ *
+ *## Projet de jeu vidéo RPG dans le cadre de notre cursus en licence Informatique
+ *
+ *## Membres du groupe :
+ *Bruneau Antoine
+ *
+ *Descomps Max
+ *
+ *Despert Ange
+ *
+ *Doneau Rafaël
+ *
+ *
+ * [Lien du dépôt GIT](https://github.com/TheMisterPenguin/ProjetL2)
+ *
+ *## La documentation
+ *
+ *[Par ici](https://themisterpenguin.github.io/Documentation_BLSD)
+ *
+ *[Version PDFLateX](https://github.com/TheMisterPenguin/ProjetL2/raw/main/doc/out/latex/refman.pdf)
+ *
+ *## Tramme GDB
+ *
+ *[Par là](https://github.com/TheMisterPenguin/ProjetL2/raw/2f5435ebd67d325b1b7616da5eba909a77823338/debogage_sanada.pdf)
+ *
+ *## Rapport Final
+ *
+ *[C'est par ici](https://github.com/TheMisterPenguin/ProjetL2/raw/main/Rapport_Projet_DESPERT_BRUNEAU_DESCOMPS_DONEAU.pdf)
+ *
+ *## GANTT
+ *
+ *[Le GANTT previsionel](https://docs.google.com/spreadsheets/d/137kKD9gfpiEC_zliMpySPo1xjQ20Uw0JYJUedu1JbkY/edit?usp=sharing)
+ *
+ *[Le GANTT réel](https://docs.google.com/spreadsheets/d/1o70FjVPzeUldbCMU4YSInlD_35gTBtZ88_-tOpvP7w8/edit?usp=sharing)
+ *
+ */
