ef595fba3fb786ff690f550d7e30349fb841bef5
TheMisterPenguin <ange.despert@gmail.com>
2022-12-29 21:05:31 +0100

    Ajout destructeur

--- src/texture.cpp
+++ src/texture.cpp
@@ -5,43 +5,46 @@
 
 namespace BloodySanada {
     class Texture {
-        private:
-            SDL_Texture *visual_texture;
-            SDL_Rect texture_selection;
-            int sizeX, sizeY;
-            int pixelsX, pixelsY;
-        
-        public:
-            Texture(const char *path_to_texture) : Texture(std::string(path_to_texture)) {}
+    private:
+        SDL_Texture *visual_texture;
+        SDL_Rect texture_selection;
+        int sizeX, sizeY;
+        int pixelsX, pixelsY;
+    
+    public:
+        Texture(const char *path_to_texture) : Texture(std::string(path_to_texture)) {}
 
-            Texture(std::string path_to_texture){
-                SDL_Surface * chargement = NULL;
+        Texture(std::string path_to_texture){
+            SDL_Surface * chargement = NULL;
 
-                /* Chargement de la texture dans une surface */
-                path_to_texture += std::string(execDir);
+            /* Chargement de la texture dans une surface */
+            path_to_texture += std::string(execDir);
 
-                log_debug("Création de la texture à partir du fichier : '%-75s', adresse : %p", path_to_texture.c_str(), this);
+            log_debug("Création de la texture à partir du fichier : '%-75s', adresse : %p", path_to_texture.c_str(), this);
 
-                chargement = SDL_LoadBMP(path_to_texture.c_str());
-                if(! chargement){
-                    warning("Erreur lors de la création de la texture : %s", ERREUR_FICHIER, SDL_GetError());
-                    throw ERREUR_FICHIER;
-                }
+            chargement = SDL_LoadBMP(path_to_texture.c_str());
+            if(! chargement){
+                warning("Erreur lors de la création de la texture : %s", ERREUR_FICHIER, SDL_GetError());
+                throw ERREUR_FICHIER;
+            }
 
-                /* Copie de la ressource dans une structure SDL_Texture */
-                visual_texture = SDL_CreateTextureFromSurface(rendu_principal, chargement);
-                SDL_FreeSurface(chargement); 
-                if(! visual_texture){
-                    warning("Erreur lors de la création de la texture : %s", ERREUR_FICHIER, SDL_GetError());
-                    free(visual_texture);
-                    throw ERREUR_FICHIER;
-                }
+            /* Copie de la ressource dans une structure SDL_Texture */
+            visual_texture = SDL_CreateTextureFromSurface(rendu_principal, chargement);
+            SDL_FreeSurface(chargement); 
+            if(! visual_texture){
+                warning("Erreur lors de la création de la texture : %s", ERREUR_FICHIER, SDL_GetError());
+                throw ERREUR_FICHIER;
+            }
 
-                if(SDL_QueryTexture(visual_texture, NULL, NULL, &pixelsX, &pixelsY)){
-                    warning("Erreur lors de la création de la texture : %s", ERREUR_FICHIER, SDL_GetError());
-                    free(visual_texture);
-                    throw ERREUR_FICHIER;
-                }   
+            if(SDL_QueryTexture(visual_texture, NULL, NULL, &pixelsX, &pixelsY)){
+                warning("Erreur lors de la création de la texture : %s", ERREUR_FICHIER, SDL_GetError());
+                throw ERREUR_FICHIER;
+            }   
+        }
+
+        ~Texture(){
+            /* On désalloue la SDL_Texture*/
+            SDL_DestroyTexture(visual_texture);
         }
     };
 } // namespace BloodySanada
